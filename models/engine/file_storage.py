fcc60129f5ebe12886b5943de64e61c02eefe54e
tegbiye <se.tegbabu.solomon@gmail.com>
2022-12-09 13:27:14 +0300

    Fixed error in file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,7 +10,7 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        if cls in None:
+        if cls is None:
             return FileStorage.__objects
         my_dicts = {}
         for key, val in FileStorage.__objects.items():
