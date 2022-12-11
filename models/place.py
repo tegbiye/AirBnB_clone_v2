c2bac8d93086ba50acde2d8c8e581eccebc84239
tegbiye <se.tegbabu.solomon@gmail.com>
2022-12-11 14:01:26 +0300

    Fixed error in place.py

--- models/place.py
+++ models/place.py
@@ -3,18 +3,18 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, ForeignKey, Float, Table
 from sqlalchemy.orm import relationship
+from models.amenity import Amenity
 
 place_amenity = Table('place_amenity', Base.metadata,
-                      Column('place_id', String(60),
-                             ForeignKey('places.id'),
+                      Column('place_id', String(60), ForeignKey('places.id'),
                              primary_key=True, nullable=False),
                       Column('amenity_id', String(60),
-                             ForeignKey('amenities.id'),
-                             primary_key=True, nullable=False)
+                             ForeignKey('amenities.id'), primary_key=True,
+                             nullable=False)
                       )
 
 
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = "places"
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
@@ -28,8 +28,8 @@ class Place(BaseModel):
     latitude = Column(Float)
     longitude = Column(Float)
     reviews = relationship("Review", backref='place', cascade="all, delete")
-    amenities = relationship(
-        "Amenity", secondary=place_amenity, viewonly=False)
+    amenities = relationship("Amenity", secondary=place_amenity,
+                             viewonly=False)
 
     @property
     def reviews(self):
