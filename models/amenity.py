053ad90727875a7661c5842155324f0a4758341d
tegbiye <se.tegbabu.solomon@gmail.com>
2022-12-12 02:00:55 +0300

    Fixed error in styling

--- models/amenity.py
+++ models/amenity.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel,Base
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.place import place_amenity
@@ -10,4 +10,4 @@ class Amenity(BaseModel, Base):
     """Amenity class that inherits from BaseModel and Base"""
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-    #place_amenities = relationship("Place", secondary=place_amenity)
+    place_amenities = relationship("Place", secondary=place_amenity)
