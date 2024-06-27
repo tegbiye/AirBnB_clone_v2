db1fa90e1d15726ba1f45fc3cc17f895fa8bd796
tegbiye <se.tegbabu.solomon@gmail.com>
2024-06-27 22:40:34 +0000

    Updated the 0-hello_route file

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,7 +7,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/airbnb-onepage/', strict_slashes=False)
 def hello_world():
     """ Returns some text. """
     return 'Hello HBNB!'
